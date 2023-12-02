b75a632ccce86562c231487b27432d8f7a22d114
Ubuntu <azureuser@myVM.bmqnon5a4u3evo4lkiiyqzknzd.bx.internal.cloudapp.net>
2023-12-02 21:03:42 +0000

    Dodanie komentarzy do pliku index.js opisujących działanie kodu

--- basic_auth_project/index.js
+++ basic_auth_project/index.js
@@ -1,43 +1,52 @@
-// Requiring module
+// Import modułu express, który umożliwia obsługę aplikacji webowej w Node.js
 const express = require("express");
+// Import modułu path, który pomaga w obsłudze ścieżek plików
 var path = require('path');
 
+// Utworzenie instancji aplikacji express
 const app = express();
 
+// Middleware do autentykacji klienta przy użyciu HTTP Basic Authentication
 function authentication(req, res, next) {
-	var authheader = req.headers.authorization;
-	console.log(req.headers);
+    // Pobranie nagłówka "Authorization" z żądania HTTP
+    var authheader = req.headers.authorization;
+    console.log(req.headers);
 
-	if (!authheader) {
-		var err = new Error('You are not authenticated!');
-		res.setHeader('WWW-Authenticate', 'Basic');
-		err.status = 401;
-		return next(err)
-	}
+    // Sprawdzenie, czy nagłówek "Authorization" został przesłany
+    if (!authheader) {
+        // Jeśli nie, utworzenie błędu i zwrócenie komunikatu o braku autentykacji
+        var err = new Error('You are not authenticated!');
+        res.setHeader('WWW-Authenticate', 'Basic');
+        err.status = 401;
+        return next(err)
+    }
 
-	var auth = new Buffer.from(authheader.split(' ')[1],
-	'base64').toString().split(':');
-	var user = auth[0];
-	var pass = auth[1];
+    // Dekodowanie danych uwierzytelniających z nagłówka "Authorization"
+    var auth = new Buffer.from(authheader.split(' ')[1], 'base64').toString().split(':');
+    var user = auth[0];
+    var pass = auth[1];
 
-	if (user == '' && pass == '') {
-
-		// If Authorized user
-		next();
-	} else {
-		var err = new Error('You are not authenticated!');
-		res.setHeader('WWW-Authenticate', 'Basic');
-		err.status = 401;
-		return next(err);
-	}
-
+    // Sprawdzenie, czy użytkownik i hasło są puste
+    if (user == '' && pass == '') {
+        // Jeśli użytkownik i hasło są puste, przechodź do kolejnego middleware
+        next();
+    } else {
+        // Jeśli użytkownik i/lub hasło są niepuste, utworzenie błędu i zwrócenie komunikatu o braku autentykacji
+        var err = new Error('You are not authenticated!');
+        res.setHeader('WWW-Authenticate', 'Basic');
+        err.status = 401;
+        return next(err);
+    }
 }
 
-// First step is the authentication of the client
+// Ustawienie middleware autentykacji
 app.use(authentication)
+
+// Ustawienie middleware do obsługi plików statycznych z katalogu 'public'
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Server setup
-app.listen((3000), () => {
-	console.log("Server is Running");
+// Konfiguracja serwera, który słucha na porcie 3000
+app.listen(3000, () => {
+    console.log("Server is Running");
 })
+
